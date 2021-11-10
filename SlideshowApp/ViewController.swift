3fd50e3176d640b2589f8533ff20b50f9ca6bf12
mesh20157 <chicken.kitten.in.the.kitchen@gmail.com>
2021-11-10 23:03:54 +0900

    スライドショーを追加

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -11,11 +11,16 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var imageView: UIImageView!
-    // indexの画像をstoryboardの画像にセットする
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var previousImageButton: UIButton!
+    @IBOutlet weak var nextImageButton: UIButton!
     
     
     // 配列に指定するindex番号を宣言
     var nowIndex:Int = 0
+    // タイマーを宣言
+    var timer : Timer!
+    
     
     // 画像の配列を宣言
     var imageArray:[UIImage] = [
@@ -29,6 +34,11 @@ class ViewController: UIViewController {
         // indexの画像をstoryboardの画像にセットする
         imageView.image = imageArray[nowIndex]
     }
+    
+
+    //拡大した画面から戻る
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+}
     
     //次の写真を表示
     @IBAction func nextImage(_ sender: Any) {
@@ -61,10 +71,52 @@ class ViewController: UIViewController {
         // indexの画像をstoryboardの画像にセットする
         imageView.image = imageArray[nowIndex]
         
-        
     }
 
-    
+    //スライドショー
+    @IBAction func slideShowButton(_ sender: Any) {
+        if (timer == nil) {
+            
+            //再生させる
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            startButton.setTitle("■",for : .normal)
+            
+            //戻るボタン&次へボタンの非表示&無効化
+            previousImageButton.isHidden = true
+            previousImageButton.isEnabled = false
+            nextImageButton.isHidden = true
+            nextImageButton.isEnabled = false
+            
+            
+        } else {
+            
+            //停止させる
+            timer.invalidate()
+            
+            timer = nil
+            startButton.setTitle("▶︎",for : .normal)
+            
+            //戻るボタン&次へボタンの表示&有効化
+            previousImageButton.isHidden = false
+            previousImageButton.isEnabled = true
+            nextImageButton.isHidden = false
+            nextImageButton.isEnabled = true
+        
+        }
+    }
+
+    @objc func changeImage() {
+        // indexを増やして表示する画像を切り替える
+        nowIndex += 1
+
+        // indexが表示予定の画像の数と同じ場合
+        if (nowIndex == imageArray.count) {
+            // indexを一番最初の数字に戻す
+            nowIndex = 0
+        }
+        // indexの画像をstoryboardの画像にセットする
+        imageView.image = imageArray[nowIndex]
+    }
     
     
 
